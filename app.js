ca5373a43bec6134522e6ca0999f7b231d42360b
sricheekati <cheekatisrilakshmi04@gmail.com>
2024-03-27 22:41:33 -0500

    all done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hRouter = require('./routes/hats');
 var gRouter = require('./routes/grid');
+var pRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hats', hRouter);
 app.use('/grid', gRouter);
+//app.use('/random', pRouter);
+app.use('/pick', pRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
